b8a79d0f695526aca7fd3562b4b8766162cab077
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-08 21:43:26 -0500

    Add new header for YTPivotBarItemView.h

--- YTPivotBarItemView.h
+++ YTPivotBarItemView.h
@@ -2,4 +2,5 @@
 
 @interface YTPivotBarItemView : UIView
 @property (strong, nonatomic) UIButton *navigationButton;
+@property (nonatomic, strong) YTPivotBarItemViewAccessibilityControl *hitTarget;
 @end
