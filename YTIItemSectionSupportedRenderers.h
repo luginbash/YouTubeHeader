146aa299b6f57a9261ee3f16b1ff5cd420f32ea1
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-08 23:57:23 -0500

    fix YTIItemSectionSupportedRenderers.h

--- YTIItemSectionSupportedRenderers.h
+++ YTIItemSectionSupportedRenderers.h
@@ -1,12 +1,14 @@
+#import "YTICompactLinkRenderer.h"
+#import "YTICompactListItemRenderer.h"
 #import "YTIElementRenderer.h"
 
 @interface YTIItemSectionSupportedRenderers : GPBMessage
+@property (readonly, nonatomic) YTICompactLinkRenderer *compactLinkRenderer;
+@property (readonly, nonatomic) YTICompactListItemRenderer *compactListItemRenderer;
 @property (nonatomic, strong, readwrite) YTIElementRenderer *elementRenderer;
 @property (nonatomic, assign, readwrite) BOOL hasPromotedVideoRenderer;
 @property (nonatomic, assign, readwrite) BOOL hasPromotedVideoInlineMutedRenderer;
 @property (nonatomic, assign, readwrite) BOOL hasCompactPromotedVideoRenderer;
-@property (readonly, nonatomic) YTICompactLinkRenderer *compactLinkRenderer;
-@property (readonly, nonatomic) YTICompactListItemRenderer *compactListItemRenderer;
 - (BOOL)hasCompactLinkRenderer;
 - (BOOL)hasCompactListItemRenderer;
 @end
