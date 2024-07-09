88e45728e3dc71d051e7443d2df82e2da32a332b
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-08 23:54:02 -0500

    Add YTICompactListItemThumbnailSupportedRenderers.h

---
+++ YTICompactListItemThumbnailSupportedRenderers.h
@@ -0,0 +1,6 @@
+#import "YTIIconThumbnailRenderer.h"
+
+@interface YTICompactListItemThumbnailSupportedRenderers : GPBMessage
+@property (nonatomic, strong) YTIIconThumbnailRenderer *iconThumbnailRenderer;
+- (BOOL)hasIconThumbnailRenderer;
+@end
