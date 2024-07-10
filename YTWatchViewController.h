da9da65c03fcb27d5f668206f3740fcf797651f0
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-10 02:37:18 -0500

    Update YTWatchViewController.h

--- YTWatchViewController.h
+++ YTWatchViewController.h
@@ -5,6 +5,7 @@
 @interface YTWatchViewController : UIViewController <YTWatchPlayerViewLayoutSource>
 @property (nonatomic, weak, readwrite) YTPlayerViewController *playerViewController;
 @property (nonatomic, strong, readwrite) YTWatchPullToFullController *pullToFullController;
+@property (nonatomic, assign, readwrite, getter=isFullscreen) BOOL fullscreen;
 - (NSUInteger)allowedFullScreenOrientations;
 - (BOOL)fullscreen;
 @end
