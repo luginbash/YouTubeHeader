2de4af54a6a2e39322ebfae5d15a4f6e8c457462
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-13 08:34:18 -0500

    Fix declarations

--- YTReelHeaderView.h
+++ YTReelHeaderView.h
@@ -8,11 +8,11 @@
 @property (nonatomic, strong) id<YTReelHeaderDelegate> delegate;
 @property (nonatomic, strong) YTReelTransparentStackView *leftStackView;
 @property (nonatomic, strong) YTReelTransparentStackView *rightStackView;
-@property (nonatomic, strong, readwrite) YTReelPlayerButton *qualityButton // Tweak: YouQuality
-- (void)addView:(id)  toStackView:(id)  withSize:(CGSize) 
+@property (nonatomic, strong, readwrite) YTReelPlayerButton *qualityButton; // Tweak: YouQuality
+- (void)addView:(id)  toStackView:(id)  withSize:(CGSize);
 - (void)addLeftStackView:(id)view toView:(id)view;
 - (void)addRightStackView:(id)view toView:(id)view;
-- (void)setRightNavigationButtonsArray:(id)
+- (void)setRightNavigationButtonsArray:(id);
 - (id<YTReelHeaderDelegate>)delegate;
 - (void)setDelegate:(id<YTReelHeaderDelegate>)delegate;
 - (void)didPressVideoQuality:(id)arg;
