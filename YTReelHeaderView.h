6e4b7eca597e71a4557f985f60e537969ddcd840
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-13 07:20:26 -0500

    Improved YTReelHeaderView Stuffs
    New stuffs to include “YTReelPlayerButton“ and additional properties.

--- YTReelHeaderView.h
+++ YTReelHeaderView.h
@@ -2,13 +2,17 @@
 #import "YTResponder.h"
 #import "YTReelHeaderDelegate.h"
 #import "YTReelTransparentStackView.h"
+#import "YTReelPlayerButton.h"
 
 @interface YTReelHeaderView : UIView <YTResponder>
 @property (nonatomic, strong) id<YTReelHeaderDelegate> delegate;
 @property (nonatomic, strong) YTReelTransparentStackView *leftStackView;
 @property (nonatomic, strong) YTReelTransparentStackView *rightStackView;
+@property (nonatomic, strong, readwrite) YTReelPlayerButton *qualityButton // Tweak: YouQuality
+- (void)addView:(id)  toStackView:(id)  withSize:(CGSize) 
 - (void)addLeftStackView:(id)view toView:(id)view;
 - (void)addRightStackView:(id)view toView:(id)view;
+- (void)setRightNavigationButtonsArray:(id)
 - (id<YTReelHeaderDelegate>)delegate;
 - (void)setDelegate:(id<YTReelHeaderDelegate>)delegate;
 - (void)didPressVideoQuality:(id)arg;
