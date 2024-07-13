01a2b8f6d6526f3b4ff8b5e04adff07eddc93810
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-13 13:08:14 -0500

    Update YTReelPlayerViewController.h

--- YTReelPlayerViewController.h
+++ YTReelPlayerViewController.h
@@ -1,8 +1,11 @@
 #import "YTPlayerViewController.h"
 #import "YTReelContentView.h"
+#import "YTReelHeaderDelegate.h"
+#import "YTResponder.h"
 
-@interface YTReelPlayerViewController : UIViewController
+@interface YTReelPlayerViewController : UIViewController <YTResponder>
 @property (nonatomic, strong, readwrite) YTPlayerViewController *player;
+- (id <YTVideoPlayerOverlayDelegate>)delegate;
 - (YTReelContentView *)contentView;
 - (NSString *)videoId;
 @end
