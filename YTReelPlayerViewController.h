a026d1fa9858b61ed23245083454bfc81c9cd9a6
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-13 13:29:15 -0500

    Update YTReelPlayerViewController.h

--- YTReelPlayerViewController.h
+++ YTReelPlayerViewController.h
@@ -6,6 +6,7 @@
 @interface YTReelPlayerViewController : UIViewController <YTResponder>
 @property (nonatomic, strong, readwrite) YTPlayerViewController *player;
 - (id <YTVideoPlayerOverlayDelegate>)delegate;
+- (void)didPressVideoQuality:(id)arg;
 - (YTReelContentView *)contentView;
 - (NSString *)videoId;
 @end
