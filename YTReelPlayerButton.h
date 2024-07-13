ec8149c0ae53666b1bed9fc1861989b65305ea06
arichornloverALT <157071384+arichornloverALT@users.noreply.github.com>
2024-07-13 07:31:33 -0500

    Added YTReelPlayerButton.h
    It’s now possible to add the buttons in Vertical just like uYou.

---
+++ YTReelPlayerButton.h
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+@interface YTReelPlayerButton : UIButton
+@property (nonatomic, assign, readwrite) NSInteger buttonAlignment;
+@property (nonatomic, assign, readwrite) BOOL applyRightSideLayoutImageSize;
+@property (nonatomic, assign, readwrite) CGFloat hitTargetInsets;
+- (void)setButtonAlignment:(NSInteger)alignment;
+- (id)imageOrAnimationView;
+- (void)layoutHorizontalButtons;
+- (void)layoutVerticalButtons;
+- (NSInteger)buttonAlignment;
+- (BOOL)applyRightSideLayoutImageSize;
+- (void)setApplyRightSideLayoutImageSize:(BOOL)size;
+- (CGFloat)hitTargetInsets;
+- (void)setHitTargetInsets:(CGFloat)insets;
+@end
