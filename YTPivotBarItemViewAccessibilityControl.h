6809d9d789b27351a74ac19c48ca5ce53952a1e4
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-10 13:57:05 -0500

    Add class to YTPivotBarItemViewAccessibilityControl.h

--- YTPivotBarItemViewAccessibilityControl.h
+++ YTPivotBarItemViewAccessibilityControl.h
@@ -1,5 +1,7 @@
 #import "YTPivotBarItemView.h"
 
+@class YTPivotBarItemView;
+
 @interface YTPivotBarItemViewAccessibilityControl : UIControl
 @property (nonatomic, strong, readwrite) YTPivotBarItemView *pivotBarItemView;
 @property (nonatomic, strong, readwrite) NSString *rendererAccessibilityLabel;
