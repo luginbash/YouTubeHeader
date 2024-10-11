ded8c022552a2a711cd1d7fb286a7068f29f88a8
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-10 22:30:30 -0500

    Update YTIPivotBarSupportedRenderers.h

--- YTIPivotBarSupportedRenderers.h
+++ YTIPivotBarSupportedRenderers.h
@@ -2,6 +2,9 @@
 #import "YTIPivotBarIconOnlyItemRenderer.h"
 
 @interface YTIPivotBarSupportedRenderers : NSObject
+@property (nonatomic, assign, readonly) int itemsOneOfCase
+@property (nonatomic, strong, readwrite) YTIPivotBarItemRenderer *pivotBarItemRenderer
+@property (nonatomic, strong, readwrite) YTIPivotBarIconOnlyItemRenderer *pivotBarIconOnlyItemRenderer
 - (YTIPivotBarItemRenderer *)pivotBarItemRenderer;
 - (YTIPivotBarIconOnlyItemRenderer *)pivotBarIconOnlyItemRenderer;
 @end
