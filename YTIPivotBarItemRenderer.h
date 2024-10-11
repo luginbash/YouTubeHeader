0b8b42e30824dff03c59fa2235d805b25fdf23fb
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-10 22:18:06 -0500

    Update YTIPivotBarItemRenderer.h

--- YTIPivotBarItemRenderer.h
+++ YTIPivotBarItemRenderer.h
@@ -3,11 +3,36 @@
 #import "YTIIcon.h"
 
 @interface YTIPivotBarItemRenderer : NSObject
-@property (nonatomic, retain) YTIIcon *icon;
+@property (nonatomic, copy, readwrite) NSString *pivotIdentifier;
+@property (nonatomic, assign, readwrite) BOOL hasPivotIdentifier;
 @property (nonatomic, copy, readwrite) NSString *targetId;
-@property (nonatomic, retain) YTICommand *navigationEndpoint;
-@property (nonatomic, copy) NSString *pivotIdentifier;
-@property (nonatomic, retain) YTIFormattedString *title;
-@property (nonatomic, copy) NSData *trackingParams;
-- (void)setNavigationEndpoint:(YTICommand *)navigationEndpoint;
+@property (nonatomic, assign, readwrite) BOOL hasTargetId;
+@property (nonatomic, strong, readwrite) YTICommand *navigationEndpoint;
+@property (nonatomic, assign, readwrite) BOOL hasNavigationEndpoint;
+@property (nonatomic, strong, readwrite) YTICommand *onSelected;
+@property (nonatomic, assign, readwrite) BOOL hasOnSelected;
+@property (nonatomic, strong, readwrite) YTICommand *onDeselected;
+@property (nonatomic, assign, readwrite) BOOL hasOnDeselected;
+@property (nonatomic, strong, readwrite) YTIFormattedString *title;
+@property (nonatomic, assign, readwrite) BOOL hasTitle;
+@property (nonatomic, strong, readwrite) YTIAccessibilitySupportedDatas *accessibility;
+@property (nonatomic, assign, readwrite) BOOL hasAccessibility;
+@property (nonatomic, assign, readonly) int imageOneOfCase;
+@property (nonatomic, strong, readwrite) YTIIcon *icon;
+@property (nonatomic, strong, readwrite) YTIThumbnailDetails *thumbnail;
+@property (nonatomic, strong, readwrite) YTIRenderer *avatar;
+@property (nonatomic, strong, readwrite) YTIPivotBarItemPresentationStyle *presentationStyle;
+@property (nonatomic, assign, readwrite) BOOL hasPresentationStyle;
+@property (nonatomic, strong, readwrite) YTIPivotBarItemIndicatorSupportedRenderers *indicator;
+@property (nonatomic, assign, readwrite) BOOL hasIndicator;
+@property (nonatomic, copy, readwrite) NSData *trackingParams;
+@property (nonatomic, assign, readwrite) BOOL hasTrackingParams;
+@property (nonatomic, assign, readwrite) int navigationType;
+@property (nonatomic, assign, readwrite) BOOL hasNavigationType;
+@property (nonatomic, strong, readwrite) YTIRenderer *progressIndicator;
+@property (nonatomic, assign, readwrite) BOOL hasProgressIndicator;
+@property (nonatomic, assign, readwrite) BOOL isDisabled;
+@property (nonatomic, assign, readwrite) BOOL hasIsDisabled;
+@property (nonatomic, strong, readwrite) YTICommand *onLongPress;
+@property (nonatomic, assign, readwrite) BOOL hasOnLongPress;
 @end
