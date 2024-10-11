1104d48fa12e512b5f2acde19f6565d09954195e
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-10 22:25:02 -0500

    Create YTPivotBarView.h

---
+++ YTPivotBarView.h
@@ -0,0 +1,21 @@
+#import <UIKit/UIKit.h>
+
+@interface YTPivotBarView : UIView
+@property (nonatomic, assign, readonly) YTPivotBarView *root;
+@property (nonatomic, strong, readwrite) UIView *separatorView;
+@property (nonatomic, strong, readwrite) UIVisualEffectView *blurView;
+@property (nonatomic, strong, readwrite) YTPivotBarItemView *itemView1;
+@property (nonatomic, strong, readwrite) YTPivotBarItemView *itemView2;
+@property (nonatomic, strong, readwrite) YTPivotBarItemView *itemView3;
+@property (nonatomic, strong, readwrite) YTPivotBarItemView *itemView4;
+@property (nonatomic, strong, readwrite) YTPivotBarItemView *itemView5;
+@property (nonatomic, strong, readwrite) YTPivotBarItemView *itemView6;
+@property (nonatomic, assign, readonly) NSArray *itemViews;
+@property (nonatomic, strong, readwrite) <YTPageStyleProvider> *pageStyleProviderDelegate;
+@property (nonatomic, weak, readwrite) <YTPivotBarItemViewDelegate> *delegate;
+@property (nonatomic, strong, readwrite) YTIPivotBarRenderer *renderer;
+@property (nonatomic, assign, readonly) UIView *contentView;
+@property (nonatomic, strong, readwrite) UIView *scrubberView;
+@property (nonatomic, assign, readonly) UIPanGestureRecognizer *scrubGestureRecognizer;
+@property (nonatomic, assign, readonly) NSInteger pageStyle;
+@end
