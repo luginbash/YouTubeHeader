8514844a99406f1931961df739e5f46be747489d
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-12 13:34:03 -0500

    Update YTIRenderer.h

--- YTIRenderer.h
+++ YTIRenderer.h
@@ -1,14 +1,10 @@
 #import <Foundation/Foundation.h>
-#import "YTILiveChatTextMessageRenderer.h"
 #import "YTIElementRenderer.h"
-#import "YTILiveChatActionPanelRenderer.h"
 #import "YTIPollRenderer.h"
 #import "YTIButtonRenderer.h"
 
 @interface YTIRenderer : GPBMessage
-@property (nonatomic, strong, readwrite) YTILiveChatTextMessageRenderer *liveChatTextMessageRenderer;
 @property (nonatomic, strong, readwrite) YTIElementRenderer *elementRenderer;
-@property (nonatomic, strong, readwrite) YTILiveChatActionPanelRenderer *liveChatActionPanelRenderer;
 @property (nonatomic, strong, readwrite) YTIPollRenderer *pollRenderer;
 @property (nonatomic, assign, readonly) NSString *stepId;
 @property (nonatomic, strong, readwrite) YTIButtonRenderer *buttonRenderer;
