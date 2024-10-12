6a9014d804b0316feba09ffa51a450320719bb81
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-12 13:43:38 -0500

    Create YTIAccessibilitySupportedDatas.h

---
+++ YTIAccessibilitySupportedDatas.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+#import "YTIAccessibilityData.h"
+
+@interface YTIAccessibilitySupportedDatas : GPBMessage
+@property (nonatomic, strong, readwrite) YTIAccessibilityData *accessibilityData;
+@end
