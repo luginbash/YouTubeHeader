018b3ef424461ef16fabf175df132ae09343e466
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-12 13:35:23 -0500

    Update YTIRenderer.h

--- YTIRenderer.h
+++ YTIRenderer.h
@@ -1,11 +1,9 @@
 #import <Foundation/Foundation.h>
 #import "YTIElementRenderer.h"
-#import "YTIPollRenderer.h"
 #import "YTIButtonRenderer.h"
 
 @interface YTIRenderer : GPBMessage
 @property (nonatomic, strong, readwrite) YTIElementRenderer *elementRenderer;
-@property (nonatomic, strong, readwrite) YTIPollRenderer *pollRenderer;
 @property (nonatomic, assign, readonly) NSString *stepId;
 @property (nonatomic, strong, readwrite) YTIButtonRenderer *buttonRenderer;
 @end
