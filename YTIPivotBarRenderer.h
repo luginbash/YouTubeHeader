ecc6bf025aaef9e50388efb67f58698b254a3645
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-16 23:07:20 -0500

    Update YTIPivotBarRenderer.h

--- YTIPivotBarRenderer.h
+++ YTIPivotBarRenderer.h
@@ -1,6 +1,9 @@
 #import "YTIPivotBarSupportedRenderers.h"
+#import "YTIPivotBarItemRenderer.h"
 
 @interface YTIPivotBarRenderer : NSObject
 @property (nonatomic, strong, readwrite) NSArray<YTIPivotBarItemRenderer *> *itemsArray;
 + (YTIPivotBarSupportedRenderers *)pivotSupportedRenderersWithBrowseId:(NSString *)browseId title:(NSString *)title iconType:(int)iconType;
+- (NSMutableArray <YTIPivotBarSupportedRenderers *> *)itemsArray;
+- (void)reorderTabsWithTabOrder:(NSArray<NSString *> *)tabOrder;
 @end
