9cd16fc44edcb7ee30e4507833eb1de0daa9eb78
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-10-21 19:23:01 -0500

    Fix YTIPivotBarRenderer.h spelling
    I accidentally typed the declaration completely wrong.

--- YTIPivotBarRenderer.h
+++ YTIPivotBarRenderer.h
@@ -2,9 +2,8 @@
 #import "YTIPivotBarItemRenderer.h"
 
 @interface YTIPivotBarRenderer : NSObject
-@property (nonatomic, strong) NSArray *itemsArray;
+@property (nonatomic, strong) NSMutableArray <YTIPivotBarSupportedRenderers *> *itemsArray;
 @property (nonatomic, strong) NSArray *tabItems;
 + (YTIPivotBarSupportedRenderers *)pivotSupportedRenderersWithBrowseId:(NSString *)browseId title:(NSString *)title iconType:(int)iconType;
-- (NSMutableArray <YTIPivotBarSupportedRenderers *> *)itemsArray;
 - (void)reorderTabsWithTabOrder:(NSArray *)tabOrder;
 @end
