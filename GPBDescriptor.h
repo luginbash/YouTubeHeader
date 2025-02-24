fa0ce6b449f4e63aec1b00289fe99aec98d2e7af
aricloverEXALT <157071384+aricloverEXALT@users.noreply.github.com>
2025-02-24 02:29:16 -0600

    Update GPBDescriptor.h

--- GPBDescriptor.h
+++ GPBDescriptor.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 
+#import "GPBExtensionDescriptor.h"
 #import "GPBRuntimeTypes.h"
 
 @class GPBEnumDescriptor;
@@ -94,20 +95,6 @@ typedef NS_ENUM(uint8_t, GPBFieldType) {
 @property(nonatomic, readonly) uint32_t enumNameCount;
 - (nullable NSString *)getEnumNameForIndex:(uint32_t)index;
 - (nullable NSString *)getEnumTextFormatNameForIndex:(uint32_t)index;
-
-@end
-
-@interface GPBExtensionDescriptor : NSObject<NSCopying>
-
-@property(nonatomic, readonly) uint32_t fieldNumber;
-@property(nonatomic, readonly) Class containingMessageClass;
-@property(nonatomic, readonly) GPBDataType dataType;
-@property(nonatomic, readonly, getter=isRepeated) BOOL repeated;
-@property(nonatomic, readonly, getter=isPackable) BOOL packable;
-@property(nonatomic, readonly) Class msgClass;
-@property(nonatomic, readonly) NSString *singletonName;
-@property(nonatomic, readonly, strong, nullable) GPBEnumDescriptor *enumDescriptor;
-@property(nonatomic, readonly, nullable) id defaultValue;
 
 @end
 
