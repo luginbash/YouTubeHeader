d573da43a9be483054fc3c7f74dd332a2246a042
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2025-03-16 00:01:40 -0500

    Update YTPlayerOverlayManager.h

--- YTPlayerOverlayManager.h
+++ YTPlayerOverlayManager.h
@@ -2,6 +2,10 @@
 #import "YTVideoPlayerOverlayDelegate.h"
 
 @interface YTPlayerOverlayManager : NSObject <YTVideoPlayerOverlayDelegate>
-- (YTVarispeedSwitchController *)varispeedController;
+@property (nonatomic, assign) float currentPlaybackRate;
+@property (nonatomic, strong, readonly) YTVarispeedSwitchController *varispeedController;
+- (void)varispeedSwitchController:(YTVarispeedSwitchController *)varispeed didSelectRate:(float)rate;
+- (void)setCurrentPlaybackRate:(float)rate;
+- (void)setPlaybackRate:(float)rate;
 - (void)didPressToggleFullscreen;
 @end
